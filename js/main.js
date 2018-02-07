b0cff293e5a27ba43930ee536a8aba41e5b6ecbc
Patrick Quan <patrickquan@Patricks-MacBook-Pro.local>
2018-02-07 14:13:06 -0700

    Improved overall layout, especially for mobile. Removed obsolete files

--- js/main.js
+++ js/main.js
@@ -1,28 +1,15 @@
+
+//Smooth scrolling
 $(document).ready(function() {
 		$('a[href]').bind('click', function(e) {
-				e.preventDefault(); // prevent hard jump, the default behavior
 
-				var target = $(this).attr("href"); // Set the target as variable
-
-				// perform animated scrolling by getting top-position of target-element and set it as scroll target
+				var target = $(this).attr("href");
 				$('html, body').stop().animate({
-						scrollTop: $(target).offset().top - 57
+						scrollTop: $(target).offset().top - 57 //Offset of scroll
 				}, 800, function() {
-						location.hash = target; //attach the hash (#jumptarget) to the pageurl
+						location.hash = target;
 				});
 
 				return false;
 		});
 });
-
-$(window).scroll(function() {
-		var scrollDistance = $(window).scrollTop();
-
-		// Assign active class to nav links while scolling
-		$('.bodyOdd').each(function(i) {
-				if ($(this).position().top <= scrollDistance) {
-						$('.nav a.active').attr('id', 'active');
-						$('.nav a').eq(i).removeAttr('id');
-				}
-		});
-}).scroll();
