3842ad5bc60c98f4f30c1b3c84b1835183bd334b
Patrick Quan <patrickquan@Patricks-MacBook-Pro.local>
2018-02-09 16:01:11 -0700

    Added open/closed status to the navbar

--- js/main.js
+++ js/main.js
@@ -14,4 +14,39 @@ $(document).ready(function() {
 		});
 });
 
-$('#myModal').modal(options)
+//Open/Closed based on time
+$(document).ready(function() {
+  // Date time variables
+  var dtNow;       // Now
+  var dtOpen;    // Weekday - Open
+  var dtCloseWD;   // Weekday - Close
+  var dtCloseFriSat;  // Saturday - Close
+  var isOpen;      // is open?
+	var openDisplay = $("#openSign");
+
+  // Initialize date values
+  dtNow = new Date();
+  dtOpen = new Date(dtNow.getFullYear(), dtNow.getMonth(), dtNow.getDate(), 11);
+  dtCloseWD = new Date(dtNow.getFullYear(), dtNow.getMonth(), dtNow.getDate(), 21);
+  dtCloseFriSat = new Date(dtNow.getFullYear(), dtNow.getMonth(), dtNow.getDate(), 24);
+  // Determine day and calculate isOpen
+  switch (dtNow.getDay()) {
+
+    case 5:
+      isOpen = (dtNow.getTime() > dtOpen.getTime() && dtNow.getTime() < dtCloseFriSat.getTime());
+      break;
+		case 6:
+			isOpen = (dtNow.getTime() > dtOpen.getTime() && dtNow.getTime() < dtCloseFriSat.getTime());
+			break;
+		default:
+			isOpen = (dtNow.getTime() > dtOpen.getTime() && dtNow.getTime() < dtCloseWD.getTime());
+  }
+
+  if (isOpen){
+		openDisplay.text(moment().format("h:mm a ddd MMM Do ") + " - Open Now")
+  }
+	else{
+		openDisplay.text(moment().format("h:mm a ddd MMM Do ") + " - Closed Now")
+
+	}
+});
