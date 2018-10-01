20a662499ea6a2f9fd7189a03a7049834f0e5fa0
Patrick Quan <patrickquan@Patricks-MacBook-Pro.local>
2018-10-01 15:43:57 -0600

    Gallery will not work until the page is fully loaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,43 +92,43 @@ if($(window).width() > 999){
 // 	}
 // }
 
+// Gallery Image Slider
+$(document).ready(function(){
+	const slide = document.querySelector('.slider');
+	const slideImages = document.querySelectorAll('.slider img');
+	const prevButton = document.querySelector('#prevButton');
+	const nextButton = document.querySelector('#nextButton');
 
+	let counter = 1;
+	const size = slideImages[0].clientWidth;
 
-//Gallery Image Slider
-const slide = document.querySelector('.slider');
-const slideImages = document.querySelectorAll('.slider img');
-const prevButton = document.querySelector('#prevButton');
-const nextButton = document.querySelector('#nextButton');
-
-let counter = 1;
-const size = slideImages[0].clientWidth;
-
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nextButton.addEventListener('click', function(){
-	if(counter >= slideImages.length - 1) return;
-	slide.style.transition = "transform 0.4s ease-in-out";
-	counter++;
 	slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
 
-prevButton.addEventListener('click', function(){
-	if(counter <= 0) return;
-	slide.style.transition = "transform 0.4s ease-in-out";
-	counter--;
-	slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-slide.addEventListener('transitionend', function(){
-	if(slideImages[counter].id === 'lastClone'){
-		slide.style.transition = "none";
-		counter = slideImages.length - 2;
+	nextButton.addEventListener('click', function(){
+		if(counter >= slideImages.length - 1) return;
+		slide.style.transition = "transform 0.4s ease-in-out";
+		counter++;
 		slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	}
+	});
 
-	if(slideImages[counter].id === 'firstClone'){
-		slide.style.transition = "none";
-		counter = slideImages.length - counter;
+	prevButton.addEventListener('click', function(){
+		if(counter <= 0) return;
+		slide.style.transition = "transform 0.4s ease-in-out";
+		counter--;
 		slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	}
-});
+	});
+
+	slide.addEventListener('transitionend', function(){
+		if(slideImages[counter].id === 'lastClone'){
+			slide.style.transition = "none";
+			counter = slideImages.length - 2;
+			slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+		}
+
+		if(slideImages[counter].id === 'firstClone'){
+			slide.style.transition = "none";
+			counter = slideImages.length - counter;
+			slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+		}
+	});
+})
