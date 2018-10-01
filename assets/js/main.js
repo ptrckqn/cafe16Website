3eee556e0b34660b16d012eaf27d0ff22d24c283
Patrick Quan <patrickquan@Patricks-MacBook-Pro.local>
2018-10-01 15:25:02 -0600

    Attempted to fixed gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
 });
 
 
-if($(window).width() > 1125){
+if($(window).width() > 999){
 	$(window).on("resize scroll", function(){
 		if($('.blankLeft').visible(true) && !$('.foodRight').visible(true)){
 			$('.drinksLeftFixed').removeAttr('id');
